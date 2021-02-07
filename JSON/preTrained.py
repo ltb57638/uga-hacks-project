e9d01f4ee2e1b53e7a52aa3427f47991133ef341
Anderson Molter <ajm70549@uga.edu>
2021-02-07 02:15:29 -0500

    Changes to Alot

--- JSON/preTrained.py
+++ JSON/preTrained.py
@@ -2,10 +2,17 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 #note: depending on how you installed (e.g., using source code download versus pip install), you may need to import like this:
 import csv
 import yfinance as yf
+import pandas
 
+import datetime
 def filterData(dates):
     with open('Scraped_data_news_output.csv', newline='') as csvfile:
         
+        # data = yf.download('AAPL','2016-01-04','2016-01-06') 
+        
+        # print(data['2016-01-04','Open'])
+        # print(data['2016-01-05','Close'])
+
         reader = csv.reader(csvfile, delimiter='|')
         compList = list(reader)
         for i in dates:
@@ -15,13 +22,19 @@ def filterData(dates):
             #get data on this ticker
             tickerData = yf.Ticker(tickerSymbol)
             #get the historical prices for this ticker
-            tickerDf = tickerData.history(period='2d', start=dates[index][1], end=dates[index][2])
+            tickerDf = yf.download(dates[index][0], start=dates[index][1], end=dates[index][2])
+            tickDict = tickerDf.to_dict()
             print(tickerDf)
+            openPrice = tickDict['Open'][dates[index][1]]
+            closePrice = tickDict['Close'][dates[index][2] - datetime.timedelta(days=1)]
+            #print(tickerDf.at[dates[index][1].date, str(dates[index][0])])
             #see your data
             analyzer = SentimentIntensityAnalyzer()
-            print(compList[index][0])
+            #print(compList[index][0])
             vs = analyzer.polarity_scores(compList[index][0])
-            print(vs)
+            
+            negValue = vs['neg']
+            posValue = vs['pos']
             
             
 
