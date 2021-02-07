8623f02a585332f5f25f7ae17d7941308366eade
Anderson Molter <ajm70549@uga.edu>
2021-02-06 22:37:45 -0500

    GetTicker works

---
+++ JSON/TickerGetter.py
@@ -0,0 +1,28 @@
+import yfinance as yf
+import string
+import os
+import csv
+import collections
+import re
+spamreader = ''
+def getTicker(content):
+    import re
+    content = re.sub(r'[^\w\s]','',content)
+    # vowels list
+    
+    articleContent = content.split()
+    articleContent = [x.strip(' ') for x in articleContent]
+    with open('constituents.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        compList = list(reader)
+        counter = collections.Counter(articleContent)
+        print(counter)
+        ticker = ''
+        lowestIndex = 0
+        for i in compList:
+            for t in articleContent:
+                #print(i[1] + ' ' + t)
+                if (i[1] == t and lowestIndex <= compList.index(i)):
+                    #print(i[1] == t)
+                    lowestIndex = compList.index(i)
+        return (compList[lowestIndex][1])
